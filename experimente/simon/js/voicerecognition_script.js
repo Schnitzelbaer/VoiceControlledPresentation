f717fdaab65b18a62f7e570eb45a1ba997ce528d
Simon Wirth <simon.julian.wirth@gmail.com>
2020-05-26 08:48:36 +0200

    Text Search Experiment

--- experimente/simon/js/voicerecognition_script.js
+++ experimente/simon/js/voicerecognition_script.js
@@ -7,10 +7,59 @@ function startRec() {
     elem.style.color = 'red';
 
     startContinuousArtyom();
-    responsiveVoice.speak("I'm listening!");
+    responsiveVoice.speak("The stage is yours!");
     $( ".status" ).fadeTo( "slow" , 1);
 
+    // slidesClassNameArray();
+}
 
+
+
+// slidesClassNameArray = function () {
+//     const slides = document.getElementsByClassName('step');
+//     const slidesArray = [];
+
+//     for (let i = 0; i < slides.length; i++) {
+//         console.log(slides[i]);
+
+//         // At this point we could also push the elements to an array
+//         slidesArray.push(slides[i]);
+//     }
+
+//     console.log(JSON.stringify(slidesArray, null, 2))
+// }
+
+
+
+
+setStartValues = function(){
+    document.getElementById('inputBackHome').value = backHome.indexes;
+    document.getElementById('inputNextSlide').value = nextSlide.indexes;
+    document.getElementById('inputPreviousSlide').value = previousSlide.indexes;
+}
+
+updateBackHome = function(){
+    var myInput = document.getElementById('inputBackHome');
+    var myData = myInput.value;    
+    var result = myData.split(',');
+
+    backHome.indexes = result;
+}
+
+updateNextSlide = function(){
+    var myInput = document.getElementById('inputNextSlide');
+    var myData = myInput.value;    
+    var result = myData.split(',');
+
+    nextSlide.indexes = result;
+}
+
+updatePreviousSlide = function(){
+    var myInput = document.getElementById('inputPreviousSlide');
+    var myData = myInput.value;    
+    var result = myData.split(',');
+
+    previousSlide.indexes = result;
 }
 
 artyom.ArtyomVoicesIdentifiers["en-US"].unshift('Google US English', 'Alex');
@@ -44,7 +93,7 @@ artyom.redirectRecognizedTextOutput(function(recognized, isFinal) {
         // abziehen des hotkeys
         recognizedVoiceInput = recognized.replace('show me', '')
         recognizedVoiceInput = recognizedVoiceInput.trim();
-        console.log("search term is: " + recognizedVoiceInput);
+        console.log("understood term: " + recognizedVoiceInput);
 
 
     } else {
@@ -60,7 +109,8 @@ var myGroup = [
         indexes: ["back to start", "start from the beginning"],
         action: function() {
             $(".intro").html("You entered: " + recognizedVoiceInput);
-          responsiveVoice.speak("You entered " + recognizedVoiceInput);
+        //   responsiveVoice.speak(recognizedVoiceInput + " Oh no, not again!");
+        responsiveVoice.speak("Let's do it again");
           var api = impress();
           api.init();
           api.goto(0);
@@ -71,7 +121,7 @@ var myGroup = [
         indexes: ["go to the next slide", "next slide please","next slide", "next please", "show me the next"],
         action: function() {
             $(".intro").html("You entered: " + recognizedVoiceInput);
-          responsiveVoice.speak("You entered " + recognizedVoiceInput);
+          responsiveVoice.speak("Next slide");
           var api = impress();
           api.init();
           api.next();
@@ -79,13 +129,66 @@ var myGroup = [
     },
 
     previousSlide = {
-        indexes: ["go back","go one back", "backt to the last slide", "back to the last", "one back", "last slide please"],
+        indexes: ["go back", "go one back", "backt to the last slide", "back to the last", "one back", "last slide please"],
         action: function() {
             $(".intro").html("You entered: " + recognizedVoiceInput);
-          responsiveVoice.speak("You entered " + recognizedVoiceInput);
+          responsiveVoice.speak("Last slide again");
           var api = impress();
           api.init();
           api.prev();
+        }
+    },
+
+    searchSlides = {
+        indexes: ["search for"],
+        action: function() {
+            $(".intro").html("You entered: " + recognizedVoiceInput);
+
+            // abziehen des hotkeys
+            recognizedVoiceInput = recognizedVoiceInput.replace('search for', '')
+            recognizedVoiceInput = recognizedVoiceInput.trim();
+            console.log("final search term is: " + recognizedVoiceInput);
+            responsiveVoice.speak("searching for: " + recognizedVoiceInput);
+
+            const options = {
+                // isCaseSensitive: false,
+                // includeScore: false,
+                // shouldSort: true,
+                // includeMatches: false,
+                // findAllMatches: false,
+                // minMatchCharLength: 1,
+                // location: 0,
+                // threshold: 0.6,
+                // distance: 100,
+                // useExtendedSearch: false,
+                keys: [
+                  "slide",
+                  "content.pTags"
+                ]
+              };
+              
+              const fuse = new Fuse(slideList, options);
+              
+              // Change the pattern
+              const pattern = recognizedVoiceInput
+
+              const searchResults = fuse.search(pattern);
+              
+            console.log(searchResults);
+
+            const indexNumber = searchResults.refIndex;
+            console.log(indexNumber);
+
+            // const key = Object.keys(searchResults)[0];
+            // value = searchResults[key]
+            // console.log(key,value);
+
+            // alert(value);
+
+            // var api = impress();
+            // api.init();
+            // api.goto(value);
+
         }
     },
 
@@ -98,5 +201,36 @@ var myGroup = [
 }
 
 ];
+
+
+const slideList = [
+    { "slide": "step box 1",
+        "content": {
+            "pTags": ["Welcome to my first shared Presentation!", "Apple", "Kiwi"],
+        }
+    },
+    { "slide": "step box 2",
+        "content": {
+            "pTags": ["This is the first slide!"],
+        }
+    },
+    { "slide": "step box 3",
+        "content": {
+            "pTags": ["This slide moves from right to left!", "Apple"],
+        }
+    },
+    { "slide": "step box 4",
+        "content": {
+            "pTags": ["This slide is in the background!"],
+        }
+    },
+    { "slide": "step box 5",
+        "content": {
+            "pTags": ["This slide has animation !"],
+        }
+    },
+]
+
+
 
 artyom.addCommands(myGroup);
